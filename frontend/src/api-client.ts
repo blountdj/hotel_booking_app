dee5189221d50cc94300de5759864bb473c6bbe1
blountdj <blountdj@gmail.com>
2024-02-27 21:57:07 +0100

    Adding filtering and sorting

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -121,6 +121,11 @@ export type SearchParams = {
     adultCount?: string;
     childCount?: string;
     page?: string;
+    facilities?: string[];
+    types?: string[];
+    stars?: string[];
+    maxPrice?: string;
+    sortOptions?: string;
 }
 
 export const searchHotels = async (searchParams: SearchParams): Promise<HotelSearchResponse> => {
@@ -131,6 +136,12 @@ export const searchHotels = async (searchParams: SearchParams): Promise<HotelSea
     queryParams.append("adultCount", searchParams.adultCount || "")
     queryParams.append("childCount", searchParams.childCount || "")
     queryParams.append("page", searchParams.page || "")
+
+    searchParams.facilities?.forEach((facility) => queryParams.append("facilities", facility))
+    searchParams.types?.forEach((type) => queryParams.append("types", type))
+    searchParams.stars?.forEach((star) => queryParams.append("stars", star))
+    queryParams.append("maxPrice", searchParams.maxPrice || "")
+    queryParams.append("sortOptions", searchParams.sortOptions || "")
 
     const response = await fetch(`${API_BASE_URL}/api/hotels/search?${queryParams}`)
 
